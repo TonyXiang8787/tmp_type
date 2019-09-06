8fc029179e044577fd3d5309bab7637f670f47f6
Tony Xiang <tonyxiang@live.nl>
2019-09-06 11:44:44 +0200

    add decode type, string not working yet

--- tmp_type/tmp.hpp
+++ tmp_type/tmp.hpp
@@ -13,7 +13,15 @@ namespace tmp {
 // max and min string length by power 2
 size_t constexpr min_str_len_pow2 = 3;
 size_t constexpr max_str_len_pow2 = 10;
+// encode string using pow2
+template<size_t l>
+struct fix_str_pow2 {
+	using type = FixStr<1ull << l>;
+};
+template<size_t l>
+using fix_str_pow2_t = typename fix_str_pow2<l>::type;
 
+// encode one digit to position
 template<size_t number, size_t position>
 struct encode_position {
 	static_assert(position < 16u, "Max position is 15!");
@@ -27,6 +35,7 @@ struct encode_position<number, 0> {
 template<size_t number, size_t position>
 uint64_t constexpr encode_position_v = encode_position<number, position>::value;
 
+// decode one digit from position
 template<uint64_t coding, size_t position>
 struct decode_position {
 	static_assert(position < 16u, "Max position is 15!");
@@ -40,6 +49,7 @@ struct decode_position<coding, 0> {
 template<uint64_t coding, size_t position>
 size_t constexpr decode_position_v = decode_position<coding, position>::value;
 
+// get scalar data type
 template<size_t type_code, size_t l = 0> struct number_to_type;
 template<size_t l> struct number_to_type<0, l> { using type = int8_t; };
 template<size_t l> struct number_to_type<1, l> { using type = uint8_t; };
@@ -54,7 +64,7 @@ template<size_t l> struct number_to_type<9, l> { using type = double; };
 template<size_t l> struct number_to_type<10, l> { 
 	static_assert(l >= min_str_len_pow2);
 	static_assert(l <= max_str_len_pow2);
-	using type = FixStr<1ull << l>; 
+	using type = fix_str_pow2_t<l>;
 };
 template<size_t type_code, size_t l = 0>
 using number_to_type_t = typename number_to_type<type_code, l>::type;
@@ -64,6 +74,30 @@ using decode_type_t = number_to_type_t<
 	decode_position_v<coding, 0>,
 	decode_position_v<coding, 2>
 >;
+
+// scalar data type to encoding
+template<class T> struct encode_type;
+template<> struct encode_type<int8_t> {
+	static size_t constexpr value = encode_position_v<0, 0>; };
+template<> struct encode_type<uint8_t> {
+	static size_t constexpr value = encode_position_v<1, 0>; };
+template<> struct encode_type<uint16_t> {
+	static size_t constexpr value = encode_position_v<2, 0>; };
+template<> struct encode_type<uint32_t> {
+	static size_t constexpr value = encode_position_v<3, 0>; };
+template<> struct encode_type<uint64_t> {
+	static size_t constexpr value = encode_position_v<4, 0>; };
+template<> struct encode_type<int16_t> {
+	static size_t constexpr value = encode_position_v<5, 0>; };
+template<> struct encode_type<int32_t> {
+	static size_t constexpr value = encode_position_v<6, 0>; };
+template<> struct encode_type<int64_t> {
+	static size_t constexpr value = encode_position_v<7, 0>; };
+template<> struct encode_type<float> {
+	static size_t constexpr value = encode_position_v<8, 0>; };
+template<> struct encode_type<double> {
+	static size_t constexpr value = encode_position_v<9, 0>; };
+
 
 using Ha = decode_type_t<encode_position_v<10, 0> + encode_position_v<5, 2>>;
 size_t constexpr a = sizeof(Ha);
